7c2bd592deb30584b5c8060ab11884905e2ed5a1
ZhangYuan <zyct@vip.qq.com>
2017-11-11 14:12:06 +0800

    更新首页

---
+++ config/urlConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    all:"http://api.taokezhushou.com/api/v1/all",
+    top_hour:"http://api.taokezhushou.com/api/v1/top_hour",
+    top_day:"http://api.taokezhushou.com/api/v1/top_day",
+    detail:"http://api.taokezhushou.com/api/v1/detail",
+    search:"http://api.taokezhushou.com/api/v1/search",
+};
